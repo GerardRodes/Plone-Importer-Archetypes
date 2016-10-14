7ae7965070b644251f08fbd703c063e6f682cc10
Gerard Rodes <grv94sk@gmail.com>
2016-10-14 12:42:05 +0200

    Update ExternalMethod.py

--- ExternalMethod.py
+++ ExternalMethod.py
@@ -6,11 +6,11 @@ def importContent(self):
   contentStructure = [
     {'contenttype': 'MyContentType'},
     {'title': 'Content {field3}'},
-    {'field': 'field1',  'type': 'String'},
-    {'field': 'field2', 'type': 'String',   'attr': 'serie_content'},
-    {'field': 'field3',  'type': 'String',   'attr': 'fecha_content',  'filter': lambda fecha, item: fecha[:4]},
-    {'field': 'field4', 'type': 'DateTime', 'attr': 'fecha_content', 'format': '%Y-%m-%d'},
-    {'field': 'file',         'type': 'File',     'attr': 'content',  'urlBuilder': lambda fileName, item: 'http://www.someurl.org/files/'+item['id']['text']+'/'+fileName }
+    {'field': 'field1',   'type': 'String'},
+    {'field': 'field2',   'type': 'String',   'attr': 'serie_content'},
+    {'field': 'field3',   'type': 'String',   'attr': 'fecha_content',  'filter': lambda fecha, item: fecha[:4]},
+    {'field': 'field4',   'type': 'DateTime', 'attr': 'fecha_content', 'format': '%Y-%m-%d'},
+    {'field': 'file',     'type': 'File',     'attr': 'content',  'urlBuilder': lambda fileName, item: 'http://www.someurl.org/files/'+item['id']['text']+'/'+fileName }
   ]
 
   importer = Importer(plonePortal       = self,
