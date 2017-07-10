d195be56b770a01dcbf796bbcec286b0f74b770c
Gerard Rodes <grv94sk@gmail.com>
2017-07-10 15:23:24 +0200

    Update Importer.py

--- Importer.py
+++ Importer.py
@@ -39,9 +39,10 @@ from Products.CMFPlone.utils import safe_unicode
 import json
 import urllib2
 import sys
+from HTMLParser import HTMLParser
+from plone import api
 
 API_KEY = "AIzaSyAopezY7RwnGO4X4xds7IV0xvBc5dp5JAg"
-
 
 class Importer:
 
@@ -49,6 +50,8 @@ class Importer:
     self.portal           = plonePortal
     self.targetFolderPath = targetFolderPath
     self.xmlPath          = xmlPath
+    self.logFileName      = datetime.now().strftime('%x %X').replace('/', '-') + '.txt'
+    self.logPath          = '/'.join(xmlPath.split('/')[:-1] + ['logs'])
     self.attrName         = attrName
     self.items            = items
     self.structure        = structure
@@ -71,6 +74,9 @@ class Importer:
                               'events': [],
                               'failedDownloads': []
                             }
+
+    if not os.path.exists(self.logPath):
+      os.makedirs(self.logPath)
 
     for field in self.structure:
       if not field.get('attr', None) and field.get('field', None):
@@ -96,17 +102,59 @@ class Importer:
 
   ##parses xml as json at .json and as tree at .tree
   def setXml(self, xmlPath = None):
-    xml  = xmlPath if xmlPath is not None else self.xmlPath
+    xml = xmlPath if xmlPath is not None else self.xmlPath
     self.addEvent('Reading xml...')
+
+    try:
+      with open(xml, "r+") as f:
+        xmlstring = f.read()
+        f.seek(0)
+        f.write(xmlstring.replace('(NULL)', ''))
+        f.truncate()
+    except:
+      pass
+
     self.tree = ET.parse(xml)
     self.addEvent('xml loaded')
     self.addEvent('Parsing xml...')
     self.json = self.parseXml(self.tree.getroot())
     self.addEvent('xml parsed to json')
 
+
+  @staticmethod
+  def createPath(path, current_folder):
+    # Folder doesn't exists, creating
+    splitted_path = path if isinstance(path, list) else path.split('/')
+    splitted_path = [folder for folder in splitted_path if folder]
+    i = 2   # 2 because /fs-database/plonesite/
+    while i < len(splitted_path):
+      if splitted_path[i] not in current_folder.keys():
+        title = ' '.join(splitted_path[i].capitalize().split('-'))
+        current_folder.invokeFactory('Folder', splitted_path[i])
+        new_folder = getattr(current_folder, splitted_path[i])
+        new_folder.setTitle(title)
+        new_folder.reindexObject()
+        transaction.commit()
+
+        print 'Created folder: ' + title
+        current_folder = new_folder
+      else:
+        current_folder = getattr(current_folder, splitted_path[i])
+
+      i += 1
+
+    return current_folder
+
+
   def setFolder(self, folderPath = None):
     path = self.targetFolderPath if folderPath is None else folderPath
-    self.targetFolder = self.portal.restrictedTraverse(path)
+
+    try:
+      self.targetFolder = self.portal.restrictedTraverse(path)
+    except Exception as e:
+      self.targetFolder = self.createPath(path, self.portal)
+
+    return self.targetFolder
 
 
 
@@ -174,9 +222,15 @@ class Importer:
           titleParts = filter(lambda part: '{' not in part and '}' not in part, titleParts)
 
           for part in titleParts:
-            attrName = filter(lambda field: field.get('field', None) == part, self.structure)[0]['attr']
+            try:
+              attrName = filter(lambda field: field.get('field', None) == part, self.structure)[0]['attr']
+            except:
+              attrName = part
             newTitle['string'] = newTitle['string'].replace('{'+part+'}',item[attrName]['text'])
-        self.addEvent('Building object: '+newTitle['string'])
+        try:
+          self.addEvent('Building object: '+str(newTitle['string']))
+        except:
+          pass
 
 
         idStructure = {'structure': filter(lambda info: info.get('id', None), self.structure)}
@@ -186,7 +240,10 @@ class Importer:
           idParts = re.compile('(?<=\{)(.*?)(?=\})').split(idStructure['structure'][0]['id'])
           idParts = filter(lambda part: '{' not in part and '}' not in part, idParts)
           for part in idParts:
-            attrName = filter(lambda field: field.get('field', None) == part, self.structure)[0]['attr']
+            try:
+              attrName = filter(lambda field: field.get('field', None) == part, self.structure)[0]['attr']
+            except:
+              attrName = part
             idStructure['string'] = idStructure['string'].replace('{'+part+'}',item[attrName]['text'])
 
           newId = plone_utils.normalizeString(safe_unicode(idStructure['string']))
@@ -225,11 +282,17 @@ class Importer:
 
         for field in fields:
           fieldValue = None
-          value = item[field['attr']].get('text', None)
+          value = ''
+          if field['attr'] in item:
+            value = item[field['attr']].get('text', None)
+
           if value != None:
             setter = getattr(newObject, 'set' + field['field'][0].capitalize() + field['field'][1:])
 
             if field['type'] == 'String':
+              fieldValue = value
+
+            elif field['type'] == 'Boolean':
               fieldValue = value
 
             elif field['type'] == 'DateTime':
@@ -246,6 +309,8 @@ class Importer:
             elif field['type'] == 'Address':
               fieldValue = self.getCoordinates(value)
               fieldValue = str(fieldValue['lat']) + '|' + str(fieldValue['lng'])
+            else:
+              fieldValue = value
 
 
             if field['type'] != 'File':
@@ -271,8 +336,10 @@ class Importer:
             self.addEvent('Success')
           except Exception as e:
             self.addEvent('Failed: ' + str(e))
-
-        newObject.reindexObject()
+        try:
+          newObject.reindexObject()
+        except:
+          pass
         self.log['stats']['success'] += 1
 
       except Exception as e:
@@ -281,6 +348,8 @@ class Importer:
         print(exc_type, fname, exc_tb.tb_lineno)
         self.addEvent('ERROR: ' + str(e) + ' \n line:' + str(exc_tb.tb_lineno))
         if getattr(targetFolder, newId, None):
+          self.addEvent('type value: '+str(type(fieldValue)))
+          self.addEvent('field value: '+str(fieldValue))
           self.addEvent('DELETING OBJECT: '+str(newId))
           targetFolder.manage_delObjects([newId])
 
@@ -288,12 +357,12 @@ class Importer:
 
 
       if i % 50 == 0:
-        self.addEvent('commit')
-        transaction.commit()
+        self.addEvent('transaction savepoint')
+        transaction.savepoint(True)
 
       self.addEvent('--- ITEM '+str(i+1)+'/'+str(self.log['stats']['total'])+' END ---')
 
-    self.addEvent('final commit')
+    self.addEvent('commit')
     transaction.commit()
 
     if self.log['stats']['fileDownloadsError'] > 0:
@@ -311,7 +380,8 @@ class Importer:
 
   def addEvent(self, event):
     print event
-    self.log['events'].append(event)
+    with open(self.logPath + '/' + self.logFileName, 'a+') as f:
+      f.write(str(datetime.now()) + ' -- ' + event + '\n')
 
 
   def downloadFile(self, field, value, item, newObject):
